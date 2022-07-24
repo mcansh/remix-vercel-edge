8c8b8350631788398ca1f4ee38008c604203093b
Logan McAnsh <logan@mcan.sh>
2022-07-24 15:28:16 -0400

    production mode
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { createRequestHandler } from "@remix-run/server-runtime";
 import * as build from "@remix-run/dev/server-build";
 
 export default async function edgeHandler(request: Request) {
-  let handler = createRequestHandler(build as any, "development");
+  let handler = createRequestHandler(build as any, "production");
   let response = await handler(request);
   return response;
 }
